0af41e59fa71ea6c41f05adc93dc7be8547982b8
Junixza <yun.briceno@gmail.com>
2019-11-02 21:24:33 -0300

    backend funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'frontend/main#index'
+
+  namespace :backend do
+    get '/', to: 'main#index'
+    resources :products
+  end
+
   scope module: :frontend do
-      get 'product', to: 'main#product'
+    get 'product', to: 'main#product'
   end
 end
